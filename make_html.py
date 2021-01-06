7dbb4317c91bf246a4ccf6a940412e1ca8b6093b
Sebastien Chiasson <sjchiass@gmail.com>
2021-01-06 16:32:00 -0500

    Adjust path

--- make_html.py
+++ make_html.py
@@ -10,7 +10,7 @@ with open("./calendar.html", "w") as f:
     for month, bg in zip([x+1 for x in range(12)],
                         ["red", "green", "blue", "green", "red", "yellow", "red", "blue", "yellow", "black", "blue", "black"]):
         cal = month_calendar(2021, month, 850, 550)
-        photo = Image.open(f"./images/2021/resized/{month:02}.jpg")
+        photo = Image.open(f"./images/2021/{month:02}.jpg")
         
         # photo = photo.resize((840, 540))
         photo = crop_resize(photo, 840, 540)
